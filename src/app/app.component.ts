85da3259d39c92b3d57697ddb44a4289a1434808
PalomequeDeLaTorre <carlospd.ti22@utsjr.edu.mx>
2025-01-27 21:25:31 -0600

    Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { DadoComponent } from './dado/dado.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [RouterOutlet, DadoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ejemplo2';
+  //propiedades
+  resultado:string = '';
+  valor1:number = this.generarAleatorio();
+  valor2:number = this.generarAleatorio();
+  valor3:number = this.generarAleatorio();
+
+
+  //metodo que genera un número aleatorio entre 1 y 6
+  generarAleatorio(){
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+
+  //función que hace la tirada y verifica el resultado
+  tirarDados(){
+    this.valor1 = this.generarAleatorio();
+    this.valor2 = this.generarAleatorio();
+    this.valor3 = this.generarAleatorio();
+
+  // Comparamos si los 3 son iguales
+  if (this.valor1 == this.valor2 && this.valor1 == this.valor3) {
+    this.resultado = 'FELICIDADES, GANASTE!!!!';
+  } else {
+    this.resultado = 'Lástima, sigue participando';
+  }
+ }
 }
+
+// crear componentes "ng generate component nombre"
+// "ng g c dado" comandos
---
+++ src/app/dado/dado.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dado',
+  standalone: true, 
+  imports: [],
+  templateUrl: './dado.component.html',
+  styleUrl: './dado.component.css'
+})
+
+
+export class DadoComponent {
+   //propiedades
+  //valor:number = Math.floor(Math.random() * 6) + 1;
+  @Input() valor: string = "";
+}
